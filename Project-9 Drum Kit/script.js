b89e0875d77a5f95a37f04c5d1d9e423af472eae
dhrumishah <84569241+dhrumishah@users.noreply.github.com>
2022-08-20 21:27:17 +0530

    Add files via upload

--- Project-9 Drum Kit/script.js
+++ Project-9 Drum Kit/script.js
@@ -1,16 +1,19 @@
+let prevElement;
+let prevAudio;
+
 function playSound(e) {
+  if (prevElement !== undefined) {
+    prevElement.classList.remove("playing");
+    prevAudio.pause();
+  }
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  prevElement = key;
+  prevAudio = audio;
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
 }
 
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  this.classList.remove("playing");
-}
-const keys = document.querySelectorAll(".key");
-keys.forEach((key) => key.addEventListener("transitioned", removeTransition));
 window.addEventListener("keydown", playSound);
